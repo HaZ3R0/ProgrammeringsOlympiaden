3c54ef0e9676c77deef8a883eb7e40dc7252c3c6
HaZ3R0 <143172143+HaZ3R0@users.noreply.github.com>
2023-11-24 11:26:34 +0100

    Add files via upload

---
+++ Uppgift1-Reduplikation/Uppgift1-Reduplikation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Uppgift1Reduplikation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ordet?");
+            string ord = Console.ReadLine();
+            Console.WriteLine("antal upprepningar?");
+            string antaltext = Console.ReadLine();
+            int antal = int.Parse(antaltext);
+
+            if (antal > 9)
+            {
+                Console.WriteLine("Du kan max skriva ordet 9 gånger");
+            }
+
+            else if (ord.Length > 9)
+            {
+                Console.WriteLine("Ordet får inte ha mer än 9 bokstäver");
+            }
+            else
+            for (int i = 0; i < antal; i++)
+            {
+                Console.Write(ord);
+            }
+        Console.ReadKey();
+        }
+    }
+}
