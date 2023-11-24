89fece30f3216d4381ac773f978842a981ff386c
HaZ3R0 <143172143+HaZ3R0@users.noreply.github.com>
2023-11-24 11:27:54 +0100

    Add files via upload

---
+++ Uppgift2-Tre i rad/Uppgift2-Tre i rad/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Uppgift2_Tre_i_rad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("N?");
+
+            string talttext = Console.ReadLine();
+            int tal = int.Parse(talttext);
+            int svar = 0;
+
+            for (int i = 1; i <= tal; i++)
+            {
+                int treigtal = (i * (i + 1) * (i + 2));
+                int sum = (svar + treigtal);
+                string sumtext = sum.ToString();
+                string sumtext2 = (sumtext + " ");
+                while (treigtal <= tal)
+                {
+                    Console.Write(sumtext2);
+                    Console.WriteLine("Du får räkna själv hur många tal det är så funkar det");
+                    break;
+                }
+            }
+        }
+    }
+}
